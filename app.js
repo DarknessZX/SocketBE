eb390fcf5d14e9300bd3760dba4d3e20645d2fa1
longlk-PC\longlk <darknesszx@gmail.com>
2017-06-28 22:03:28 +0700

    socket

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 })
 
+let messageObject = {
+  message : 'welcome everyone',
+  time : new Date()
+};
+
+let broadcastMessage = {
+  message : 'broadcast Message',
+  time : new Date()
+}
+
 io.on('connection', (socket) => {
   console.log('user connect');
   console.log(socket.id);
@@ -18,6 +28,16 @@ io.on('connection', (socket) => {
   socket.on('welcomeMessage', (message) => {
     console.log(message);
     socket.emit('serverWelcome', 'welcome');
+  });
+
+  io.emit('chatAll', messageObject);
+
+  socket.broadcast.emit('boardcastMessage', broadcastMessage);
+
+  socket.on('joinRoom', (roomName) => {
+    console.log(roomName);
+    socket.join(roomName);
+    io.to(roomName).emit('roomMessage',`${socket.id} join ${roomName}`);
   });
 })
 
--- public/js/index.js
+++ public/js/index.js
@@ -5,5 +5,24 @@ socket.on('connect', ()=> {
 
   socket.on('serverWelcome', (message) => {
     console.log(message);
-  })
+  });
+});
+
+socket.on('chatAll', (messageObject) => {
+  console.log(messageObject);
+});
+
+socket.on('boardcastMessage', (boardcastMessage) => {
+  console.log(boardcastMessage);
 })
+
+socket.on('roomMessage', (roomMessage) => {
+  console.log(roomMessage);
+})
+
+var buttonId = document.getElementById('submit_button');
+var inputId = document.getElementById('room_name');
+
+buttonId.onclick = () => {
+  socket.emit('joinRoom', inputId.value);
+}
